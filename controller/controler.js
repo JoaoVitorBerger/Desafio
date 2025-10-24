fc1df11dcf27c1f80bba2a6fb544b35d42a1a399
JoaoVitorBerger <126708001+JoaoVitorBerger@users.noreply.github.com>
2025-10-23 23:44:23 -0300

    Projeto com os requisitos mínimos funcionais

--- controller/controler.js
+++ controller/controler.js
@@ -18,6 +18,13 @@ const controler = {
     renderHomeInscricao: (req,res)=>{
         res.render('inscricao')
     },
+     renderHomeSobreCursos: (req,res)=>{
+        res.render('aboutUser')
+    },
+    renderHomeSobreAlunos: (req,res)=>{
+        res.render('aboutCourse')
+    },
+
     createAluno: (req,res)=>{
         //Criar um aluno com as informações que vem do send do form e inserir no banco
         let {nome, mail} = req.body
@@ -27,14 +34,12 @@ const controler = {
                 return res.render('message', {message: "Aluno já cadastrado", caminho: "/homeAluno"})
             }
         }
-
         const novoAluno = Aluno.criar(normalizando_nome, mail)
         ValuesAlunos.push(novoAluno)
         console.log(ValuesAlunos)
         res.redirect('/homeAluno')
     },
     createCourse: (req,res)=>{
-        
         let{nome_curso, descricao_curso} = req.body
         let normalizando_curso = nome_curso.trim().replaceAll(" ", "_").toUpperCase()
         let normalizando_descricao = descricao_curso.trim().replaceAll(" ", "_").toUpperCase()
@@ -55,21 +60,55 @@ const controler = {
     let alunoEncontrado = ValuesAlunos.find(aluno => aluno.nome === normalizando_nome_aluno)
     let cursoEncontrado = ValuesCursos.find(curso => curso.nome === normalizando_nome_curso)
 
-    if(alunoEncontrado == false || cursoEncontrado == false){
+    if(alunoEncontrado == undefined || cursoEncontrado == undefined){
         return res.render('message', {message: "Aluno ou curso não encontrado", caminho: "/homeInscricao"})
     }
-    console.log(alunoEncontrado)
-    console.log(cursoEncontrado)
-    if (ValuesInscricoes.find(aluno => aluno.registroAcademico === alunoEncontrado.registroAcademico && aluno.cursosInscritos.includes(cursoEncontrado.codigoCurso))){
+    //console.log(alunoEncontrado, "aluno")
+    //console.log(cursoEncontrado, "curso")
+    if (ValuesInscricoes.find(inscrito => inscrito.aluno === alunoEncontrado.registroAcademico && inscrito.curso.includes(cursoEncontrado.codigoCurso))){
         return res.render('message', {message: "Aluno já inscrito nesse curso", caminho: "/homeInscricao"})
     }
-    
-    ValuesInscricoes.push({aluno: alunoEncontrado, curso: cursoEncontrado})
+    for (const aluno of ValuesAlunos){
+        if (aluno.registroAcademico === alunoEncontrado.registroAcademico){
+            aluno.cursosInscritos.push(cursoEncontrado.codigoCurso)
+        }
+    }
+    for (const curso of ValuesCursos){
+        if (curso.codigoCurso === cursoEncontrado.codigoCurso){
+            //console.log(alunoEncontrado.registroAcademico)
+            curso.alunosIncritos.push(alunoEncontrado.registroAcademico)
+        }
+    }
+    ValuesInscricoes.push({aluno: alunoEncontrado.registroAcademico, curso: cursoEncontrado.codigoCurso})
     console.log(ValuesInscricoes)
     res.render('message', {message: "Aluno cadastrado no curso com sucesso", caminho: "/homeInscricao"})
-}
+    
+   
+},
+    exibirInfoCurso : (req,res)=>{
+        // id do curso
+        // pesquisar no array de cursos se o curso existe, caso ele exista vou solicitar as informações dos alunos inscritos nele e enviar para o meu template
+        let idCurso = req.params.codigoCurso
+        //console.log(idCurso)
+        let infoCurso = ValuesCursos.find(curso=>curso.codigoCurso == idCurso)
+        let infoAlunos = ValuesAlunos.filter(aluno=>infoCurso.alunosIncritos.includes(aluno.registroAcademico))
+        console.log(infoAlunos)
+        return res.json(infoAlunos);
+       
+        
+    },
+    exibirInfoAluno : (req,res)=>{
+        //nome do aluno
+        // pesquisar no array e trazer todas as informações sobre os cursos em que o aluno está inscrito
+        let idAluno = req.params.registroAluno
+        console.log(idAluno)
+        let infoAluno = ValuesAlunos.find(aluno=>aluno.registroAcademico == idAluno)
+        console.log(infoAluno)
+        let infoCursosAluno = ValuesCursos.filter(curso=>infoAluno.cursosInscritos.includes(curso.codigoCurso))
+        console.log(infoCursosAluno)
+        return res.json(infoCursosAluno);
 
-
+    }
 
 }
 module.exports = controler
--- routes.js
+++ routes.js
@@ -8,6 +8,12 @@ router.get('/', controler.renderHome)
 router.get('/homeAluno', controler.renderHomeAluno)
 router.get('/homeCurso', controler.renderHomeCurso)
 router.get('/homeInscricao', controler.renderHomeInscricao)
+router.get('/homeSobreCursos', controler.renderHomeSobreCursos)
+router.get('/homeSobreAlunos', controler.renderHomeSobreAlunos)
+
+
+router.get('/inscricao/aluno/:registroAluno', controler.exibirInfoAluno)
+router.get('/inscricao/curso/:codigoCurso', controler.exibirInfoCurso)
 
 router.post('/aluno/create', controler.createAluno)
 router.post('/curso/create', controler.createCourse)
